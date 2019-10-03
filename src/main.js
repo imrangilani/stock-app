22d4e539186571c5d615d446f29752e886d0a80a
Imran Gilani <imran.gilani@bloomingdales.com>
2019-10-03 17:27:17 -0400

    Adding components

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import { routes } from './routes'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 Vue.config.productionTip = false
 
 const router = new VueRouter({
