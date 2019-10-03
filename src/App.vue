77db34b4b04b093e9e5b132988ba30a224abe266
Imran Gilani <imran.gilani@bloomingdales.com>
2019-10-03 17:49:13 -0400

    Adding components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="container">
-    <router-view></router-view>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <app-header></app-header>
+    <div class="col-xs-12">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 <script>
+import Header from './components/Header.vue';
 export default {
+    components: {
+      appHeader: Header
+    }
 }
 </script>
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1>The Header Component</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+         <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <router-link class="nav-link" to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
+                <router-link class="nav-link" to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>  
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" 
+                        href="#" id="navbarDropdown" 
+                        role="button" 
+                        data-toggle="dropdown" 
+                        aria-haspopup="true" 
+                        aria-expanded="false">
+                    Save & Load
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </template>
